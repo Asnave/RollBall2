bca9495f57178bf5fde90694c1929f126311f560
Abigail Evans <cdn_abigail@outlook.com>
2021-10-22 11:04:02 -0300

    Pause Menu Quit Button working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,9 @@ public class MainMenu : MonoBehaviour
 {
   public Animator transition;
   public float transitionTime = 1f;
+
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
   public void PlayGame ()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
@@ -31,5 +34,33 @@ public class MainMenu : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadScene(levelIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+   public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
